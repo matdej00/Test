cc71ce5c1fc1e29b391deab1fe8341729853737b
deki <matdej00@gmail.com>
2013-11-03 14:58:41 +0100

    Test.java dazu

---
+++ Test.java
@@ -0,0 +1,11 @@
+package cs107;
+
+public class Test {
+	public static void main(String[] args) {
+		String name = "cs107";
+		if (args.length > 0) {
+			name = args[0];
+		}
+		System.out.printf("Hello %s\n", name);
+		}
+}
